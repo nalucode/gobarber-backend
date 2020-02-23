ea740e847020ed63c244fc4d014496627dfbfc35
Luan Vinicius <nalucode@hotmail.com>
2020-02-23 06:28:14 -0300

    create SessionController & route create Session

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '48c4da8275231b03fc18994806df46ab',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/Controllers/UserController';
+import SessionController from './app/Controllers/SessionController';
 
 const routes = new Router();
 
-routes.post('/user', UserController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
