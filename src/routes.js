560e8c5ee19816dc9d13d37fb69aae58f0b48c2d
Luan Vinicius <nalucode@hotmail.com>
2020-02-23 06:48:00 -0300

    create authorization middleware

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import UserController from './app/Controllers/UserController';
-import SessionController from './app/Controllers/SessionController';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
