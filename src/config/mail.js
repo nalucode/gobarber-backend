17f9cf2d37742bdb263a87f8cb21cd6030927c57
Luan Vinicius <nalucode@hotmail.com>
2020-02-28 05:55:57 -0300

    config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fd52eea6cc0329',
+    pass: 'dbbd5b3160048f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
